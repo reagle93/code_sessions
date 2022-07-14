9d2ef9cadc909595709872c3c2777abc2a877f50
reagle93 <89871532+reagle93@users.noreply.github.com>
2022-07-14 11:34:43 -0400

    Create sorted_seq_3.cpp

---
+++ sorted_seq_3.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void find3Numbers(vector<int>& nums)
+{
+	
+  if (nums.size() < 3){
+      cout << "No such triplet found";
+      return;
+  }
+
+  int seq_max_val = INT_MAX;	
+  int seq_min_val = nums[0];
+  int min_num = nums[0];
+
+  for (int i = 1; i < nums.size(); i++)
+  {
+      if (nums[i] < min_num) {
+          min_num = nums[i];
+      }
+      else if(nums[i] > min_num) {
+
+          if (nums[i] < seq_max_val) {	
+            seq_max_val = nums[i];	// seq_max_val as low as possible
+            seq_min_val = min_num;	// working min		
+          }
+
+          else if (nums[i] > seq_max_val)
+          {	
+              cout << "Triplet: " << seq_min_val <<
+                      ", " << seq_max_val << ", " <<
+                              nums[i] << endl;
+              return;
+          }
+      } 
+  }
+  cout << "No such triplet found";
+}
+
+int main() {
+  vector<int> nums {1,2,-1,7,5};
+  find3Numbers(nums);
+}
