52bea0c1c77b4d2ec1ea4441c2d690365083a889
reagle93 <89871532+reagle93@users.noreply.github.com>
2022-07-18 19:35:02 -0400

    Create n_queen.cpp

---
+++ n_queen.cpp
@@ -0,0 +1,59 @@
+#include <bits/stdc++.h>
+
+#define breakLine cout << "\n---------------------------------\n";
+#define MAX 10
+
+using namespace std;
+
+int arr[MAX], no;
+
+void nQueens(int k, int n);
+bool canPlace(int k, int i);
+void display(int n);
+
+void nQueens(int col, int n){
+
+	for (int i = 1;i <= n;i++){
+		if (canPlace(col, i)){
+			arr[col] = i;
+			if (col == n)
+				display(n);
+			else
+				nQueens(col + 1, n);
+		}
+	}
+}
+
+bool canPlace(int col, int i){
+	for (int j = 1;j <= col - 1;j++){
+		if (arr[j] == i ||
+			(abs(arr[j] - i) == abs(j - col))) //same row or diagnonal
+		return false;
+	}
+	return true;
+}
+
+void display(int n){
+	breakLine
+	cout << "Arrangement No. " << ++no;
+	breakLine
+
+	for (int i = 1; i <= n; i++){
+		for (int j = 1; j <= n; j++){
+			if (arr[i] != j)
+				cout << "\t_";
+			else
+				cout << "\tQ";
+		}
+		cout << endl;
+	}
+
+	breakLine
+}
+
+// Driver Code
+int main(){
+	int n = 4;
+	nQueens(1, n);
+	return 0;
+}
